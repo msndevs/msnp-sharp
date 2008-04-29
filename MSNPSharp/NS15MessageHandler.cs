41a02143e0397f5a2cb8634a332d692751465d5f
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2008-04-29 09:23:06 +0000

    Group obsoleted methods.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk/MSNP-Sharp15@147 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/NS15MessageHandler.cs
+++ MSNPSharp/NS15MessageHandler.cs
@@ -440,27 +440,9 @@ namespace MSNPSharp
         internal Owner owner = new Owner();
 
         /// <summary>
-        /// Tracks the last contact object which has been synchronized. Used for BPR commands.
-        /// </summary>
-        [Obsolete]
-        private Contact lastContactSynced = null;
-
-        /// <summary>
-        /// Tracks the number of LST commands to expect
-        /// </summary>
-        [Obsolete]
-        private int syncContactsCount = 0;
-
-        /// <summary>
         /// Keep track whether a address book synchronization has been completed so we can warn the client programmer
         /// </summary>
         private bool abSynchronized = false;
-
-        /// <summary>
-        /// Keeps track of the last received synchronization identifier
-        /// </summary>
-        [Obsolete]
-        private int lastSync = -1;
 
         /// <summary>
         /// Defines whether the user is signed in the messenger network
@@ -471,7 +453,6 @@ namespace MSNPSharp
         /// A collection of all switchboard handlers waiting for a connection
         /// </summary>
         private Queue pendingSwitchboards = new Queue();
-
         
 
         /// <summary>
@@ -1024,45 +1005,6 @@ namespace MSNPSharp
             }
         }
 
-        
-
-
-        #region Contact & Group Operations [Obsolete]
-
-        /// <summary>
-        /// Creates a new contact and sends a request to the server to add this contact to the forward and allowed list.
-        /// </summary>
-        /// <param name="account">An e-mail adress to add</param>
-        [Obsolete("Use ContactList.AddNewContact")]
-        public virtual void AddNewContact(string account)
-        {
-            ContactList.AddContactGroup(account);
-        }
-
-        /// <summary>
-        /// Remove the specified contact from your forward and allow list. Note that remote contacts that are blocked remain blocked.
-        /// </summary>
-        /// <param name="contact">Contact to remove</param>
-        [Obsolete("Use ContactList.RemoveContact")]
-        public virtual void RemoveContact(Contact contact)
-        {
-            ContactList.RemoveContact(contact);
-        }
-
-        [Obsolete("Use ContactList.AddContactToGroup")]
-        public virtual void AddContactToGroup(Contact contact, ContactGroup group)
-        {
-            ContactList.AddContactToGroup(contact, group);
-        }
-
-        [Obsolete("Use ContactList.RemoveContactFromGroup")]
-        public virtual void RemoveContactFromGroup(Contact contact, ContactGroup group)
-        {
-            ContactList.RemoveContactFromGroup(contact, group);
-        }
-
-        #endregion
-
         /// <summary>
         /// Send the server a request for the contact's screen name.
         /// </summary>
@@ -1077,29 +1019,6 @@ namespace MSNPSharp
                 throw new InvalidOperationException("This is not a valid Messenger contact.");
 
             MessageProcessor.SendMessage(new NSMessage("SBP", new string[] { contact.Guid, "MFN", HttpUtility.UrlEncode(contact.Name) }));
-        }
-
-        
-        /// <summary>
-        /// Block this contact. This way you don't receive any messages anymore. This contact
-        /// will be removed from your allow list and placed in your blocked list.
-        /// </summary>
-        /// <param name="contact">Contact to block</param>
-        [Obsolete("Use ContactList.BlockContact")]
-        public virtual void BlockContact(Contact contact)
-        {
-            ContactList.BlockContact(contact);
-        }
-
-        /// <summary>
-        /// Unblock this contact. After this you are able to receive messages from this contact. This contact
-        /// will be removed from your blocked list and placed in your allowed list.
-        /// </summary>
-        /// <param name="contact">Contact to unblock</param>
-        [Obsolete("Use ContactList.UnBlockContact")]
-        public virtual void UnBlockContact(Contact contact)
-        {
-            ContactList.UnBlockContact(contact);
         }
 
         /// <summary>
@@ -2042,89 +1961,7 @@ namespace MSNPSharp
         }
 
 
-        /// <summary>
-        /// Called when a LST command has been received.
-        /// </summary>
-        /// <remarks>
-        /// Indicates that the server has send either a forward, reverse, blocked or access list.
-        /// <code>LST [Contact Guid] [List Bit] [Display Name] [Group IDs]</code>
-        /// </remarks>
-        /// <param name="message"></param>
-        [Obsolete]
-        protected virtual void OnLSTReceived(NSMessage message)
-        {
-            // decrease the number of LST commands following the SYN command we can expect
-            syncContactsCount--;
-
-            int indexer = 0;
-
-            //contact email					
-            string _contact = message.CommandValues[indexer].ToString();
-            Contact contact = ContactList.GetContact(_contact.Remove(0, 2));
-            contact.NSMessageHandler = this;
-            indexer++;
-
-            // store this contact for the upcoming BPR commands			
-            lastContactSynced = contact;
-
-            //contact name
-            if (message.CommandValues.Count > 3)
-            {
-                string name = message.CommandValues[indexer].ToString();
-                contact.SetName(name.Remove(0, 2));
-
-                indexer++;
-            }
-
-            if (message.CommandValues.Count > indexer && message.CommandValues[indexer].ToString().Length > 2)
-            {
-                string guid = message.CommandValues[indexer].ToString().Remove(0, 2);
-                contact.SetGuid(guid);
-
-                indexer++;
-            }
-
-            //contact list				
-            if (message.CommandValues.Count > indexer)
-            {
-                try
-                {
-                    int lstnum = int.Parse(message.CommandValues[indexer].ToString());
-
-                    contact.SetLists((MSNLists)lstnum);
-                    indexer++;
-
-                }
-                catch (System.FormatException)
-                {
-                }
-            }
-
-            if (message.CommandValues.Count > indexer)
-            {
-                string[] groupids = message.CommandValues[indexer].ToString().Split(new char[] { ',' });
-
-                foreach (string groupid in groupids)
-                    if (groupid.Length > 0 && contactGroups[groupid] != null)
-                        contact.ContactGroups.Add(contactGroups[groupid]); //we add this way so the event doesn't get fired
-            }
-
-            // if all LST commands are send in this synchronization cyclus we call the callback
-            if (syncContactsCount <= 0)
-            {
-                // set this so the user can set initial presence
-                abSynchronized = true;
-
-                if (AutoSynchronize == true)
-                    OnSignedIn();
-
-                // call the event
-                if (SynchronizationCompleted != null)
-                {
-                    SynchronizationCompleted(this, new EventArgs());
-                }
-            }
-        }
+        
 
         /// <summary>
         /// Gets a new switchboard handler object. Called when a remote client initiated the switchboard.
@@ -2344,54 +2181,8 @@ namespace MSNPSharp
         /// <param name="message"></param>
         protected virtual void OnLSGReceived(NSMessage message)
         {
-            ContactGroups.AddGroup(
-                new ContactGroup((string)message.CommandValues[0], (string)message.CommandValues[1], this));
+            ContactGroups.AddGroup(new ContactGroup((string)message.CommandValues[0], (string)message.CommandValues[1], this));
         }
-
-
-        /// <summary>
-        /// Called when a SYN command has been received.
-        /// </summary>
-        /// <remarks>
-        /// Indicates that the server has send an answer to a request for a list synchronization from the client. It stores the 
-        /// parameters of the command internally for future use with other commands.
-        /// Raises the SynchronizationCompleted event when there are no contacts on any list.
-        /// <code>SYN [Transaction] [Cache] [Cache] [Contact count] [Group count]</code>
-        /// </remarks>
-        /// <param name="message"></param>
-        [Obsolete]
-        protected virtual void OnSYNReceived(NSMessage message)
-        {
-            int syncNr = int.Parse((string)message.CommandValues[0], System.Globalization.CultureInfo.InvariantCulture);
-
-            syncContactsCount = int.Parse((string)message.CommandValues[3], System.Globalization.CultureInfo.InvariantCulture);
-
-            // check whether there is a new list version or no contacts are on the list
-            if (lastSync == syncNr || syncContactsCount == 0)
-            {
-                syncContactsCount = 0;
-                // set this so the user can set initial presence
-                abSynchronized = true;
-
-                if (AutoSynchronize == true)
-                    OnSignedIn();
-
-                // no contacts are sent so we are done synchronizing
-                // call the callback
-                // MSNP8: New callback defined, SynchronizationCompleted.
-                if (SynchronizationCompleted != null)
-                {
-                    SynchronizationCompleted(this, new EventArgs());
-                    abSynchronized = true;
-                }
-            }
-            else
-            {
-                lastSync = syncNr;
-                lastContactSynced = null;
-            }
-        }
-
 
         /// <summary>
         /// Called when a ADC command has been received.
@@ -3091,6 +2882,208 @@ namespace MSNPSharp
         #endregion
 
         #endregion
+
+        #endregion
+
+        #region [[[Obsolete]]]
+
+        /// <summary>
+        /// Creates a new contact and sends a request to the server to add this contact to the forward and allowed list.
+        /// </summary>
+        /// <param name="account">An e-mail adress to add</param>
+        [Obsolete("Use ContactList.AddNewContact")]
+        public virtual void AddNewContact(string account)
+        {
+            ContactList.AddNewContact(account);
+        }
+
+        /// <summary>
+        /// Remove the specified contact from your forward and allow list. Note that remote contacts that are blocked remain blocked.
+        /// </summary>
+        /// <param name="contact">Contact to remove</param>
+        [Obsolete("Use ContactList.RemoveContact")]
+        public virtual void RemoveContact(Contact contact)
+        {
+            ContactList.RemoveContact(contact);
+        }
+
+        [Obsolete("Use ContactList.AddContactToGroup")]
+        public virtual void AddContactToGroup(Contact contact, ContactGroup group)
+        {
+            ContactList.AddContactToGroup(contact, group);
+        }
+
+        [Obsolete("Use ContactList.RemoveContactFromGroup")]
+        public virtual void RemoveContactFromGroup(Contact contact, ContactGroup group)
+        {
+            ContactList.RemoveContactFromGroup(contact, group);
+        }
+
+        /// <summary>
+        /// Block this contact. This way you don't receive any messages anymore. This contact
+        /// will be removed from your allow list and placed in your blocked list.
+        /// </summary>
+        /// <param name="contact">Contact to block</param>
+        [Obsolete("Use ContactList.BlockContact")]
+        public virtual void BlockContact(Contact contact)
+        {
+            ContactList.BlockContact(contact);
+        }
+
+        /// <summary>
+        /// Unblock this contact. After this you are able to receive messages from this contact. This contact
+        /// will be removed from your blocked list and placed in your allowed list.
+        /// </summary>
+        /// <param name="contact">Contact to unblock</param>
+        [Obsolete("Use ContactList.UnBlockContact")]
+        public virtual void UnBlockContact(Contact contact)
+        {
+            ContactList.UnBlockContact(contact);
+        }
+        /// <summary>
+        /// Tracks the last contact object which has been synchronized. Used for BPR commands.
+        /// </summary>
+        [Obsolete]
+        private Contact lastContactSynced = null;
+
+        /// <summary>
+        /// Tracks the number of LST commands to expect
+        /// </summary>
+        [Obsolete]
+        private int syncContactsCount = 0;
+
+        /// <summary>
+        /// Keeps track of the last received synchronization identifier
+        /// </summary>
+        [Obsolete]
+        private int lastSync = -1;
+
+        /// <summary>
+        /// Called when a LST command has been received.
+        /// </summary>
+        /// <remarks>
+        /// Indicates that the server has send either a forward, reverse, blocked or access list.
+        /// <code>LST [Contact Guid] [List Bit] [Display Name] [Group IDs]</code>
+        /// </remarks>
+        /// <param name="message"></param>
+        [Obsolete]
+        protected virtual void OnLSTReceived(NSMessage message)
+        {
+            // decrease the number of LST commands following the SYN command we can expect
+            syncContactsCount--;
+
+            int indexer = 0;
+
+            //contact email					
+            string _contact = message.CommandValues[indexer].ToString();
+            Contact contact = ContactList.GetContact(_contact.Remove(0, 2));
+            contact.NSMessageHandler = this;
+            indexer++;
+
+            // store this contact for the upcoming BPR commands			
+            lastContactSynced = contact;
+
+            //contact name
+            if (message.CommandValues.Count > 3)
+            {
+                string name = message.CommandValues[indexer].ToString();
+                contact.SetName(name.Remove(0, 2));
+
+                indexer++;
+            }
+
+            if (message.CommandValues.Count > indexer && message.CommandValues[indexer].ToString().Length > 2)
+            {
+                string guid = message.CommandValues[indexer].ToString().Remove(0, 2);
+                contact.SetGuid(guid);
+
+                indexer++;
+            }
+
+            //contact list				
+            if (message.CommandValues.Count > indexer)
+            {
+                try
+                {
+                    int lstnum = int.Parse(message.CommandValues[indexer].ToString());
+
+                    contact.SetLists((MSNLists)lstnum);
+                    indexer++;
+
+                }
+                catch (System.FormatException)
+                {
+                }
+            }
+
+            if (message.CommandValues.Count > indexer)
+            {
+                string[] groupids = message.CommandValues[indexer].ToString().Split(new char[] { ',' });
+
+                foreach (string groupid in groupids)
+                    if (groupid.Length > 0 && contactGroups[groupid] != null)
+                        contact.ContactGroups.Add(contactGroups[groupid]); //we add this way so the event doesn't get fired
+            }
+
+            // if all LST commands are send in this synchronization cyclus we call the callback
+            if (syncContactsCount <= 0)
+            {
+                // set this so the user can set initial presence
+                abSynchronized = true;
+
+                if (AutoSynchronize == true)
+                    OnSignedIn();
+
+                // call the event
+                if (SynchronizationCompleted != null)
+                {
+                    SynchronizationCompleted(this, new EventArgs());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when a SYN command has been received.
+        /// </summary>
+        /// <remarks>
+        /// Indicates that the server has send an answer to a request for a list synchronization from the client. It stores the 
+        /// parameters of the command internally for future use with other commands.
+        /// Raises the SynchronizationCompleted event when there are no contacts on any list.
+        /// <code>SYN [Transaction] [Cache] [Cache] [Contact count] [Group count]</code>
+        /// </remarks>
+        /// <param name="message"></param>
+        [Obsolete]
+        protected virtual void OnSYNReceived(NSMessage message)
+        {
+            int syncNr = int.Parse((string)message.CommandValues[0], System.Globalization.CultureInfo.InvariantCulture);
+
+            syncContactsCount = int.Parse((string)message.CommandValues[3], System.Globalization.CultureInfo.InvariantCulture);
+
+            // check whether there is a new list version or no contacts are on the list
+            if (lastSync == syncNr || syncContactsCount == 0)
+            {
+                syncContactsCount = 0;
+                // set this so the user can set initial presence
+                abSynchronized = true;
+
+                if (AutoSynchronize == true)
+                    OnSignedIn();
+
+                // no contacts are sent so we are done synchronizing
+                // call the callback
+                // MSNP8: New callback defined, SynchronizationCompleted.
+                if (SynchronizationCompleted != null)
+                {
+                    SynchronizationCompleted(this, new EventArgs());
+                    abSynchronized = true;
+                }
+            }
+            else
+            {
+                lastSync = syncNr;
+                lastContactSynced = null;
+            }
+        }
 
         #endregion
     }
