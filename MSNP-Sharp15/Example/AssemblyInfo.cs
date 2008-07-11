ac4422cbe21db0c9e9857fe2f88cbae72fc9f54d
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2008-07-11 09:19:42 +0000

    Fix for StorageService connection. Folder is new ServiceType and it is used for Files & Documents. Enums are very strict :(
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk@356 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNP-Sharp15/Example/AssemblyInfo.cs
+++ MSNP-Sharp15/Example/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
-[assembly: AssemblyVersionAttribute("2.0.2.*")]
+[assembly: AssemblyVersionAttribute("2.0.1.*")]
 [assembly: AssemblyTitleAttribute("MSNPSharp Example client")]
 [assembly: AssemblyDescriptionAttribute(".NET MSN Messenger library Example Client")]
 [assembly: AssemblyCopyrightAttribute("Copyright (c) 2002-2006 Xih Solutions [b.geertsema@xihsolutions.net]")]
