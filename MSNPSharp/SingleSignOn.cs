f0c3239403b2f6def4323068dfc56d093e0f20d5
freezingsoft <freezingsoft@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-01-17 10:55:15 +0000

    reverse r706
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_25_STABLE@713 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/SingleSignOn.cs
+++ MSNPSharp/SingleSignOn.cs
@@ -581,7 +581,7 @@ namespace MSNPSharp
             {
                 if (user.Split('@')[1].ToLower(CultureInfo.InvariantCulture) == "msn.com")
                 {
-                    securService.Url = @"https://msnia.login.live.com/RST2.srf";
+                    securService.Url = @"https://msnia.login.live.com/pp550/RST.srf";
                 }
             }
             else
