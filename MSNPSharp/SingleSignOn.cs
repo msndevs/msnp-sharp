35b5e44e6507c3c98577ae3bdf2d73c607e4ba13
freezingsoft <freezingsoft@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-01-17 12:18:24 +0000

    The url that MSN8.5 used. No need to handle redirect.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_25_STABLE@716 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/SingleSignOn.cs
+++ MSNPSharp/SingleSignOn.cs
@@ -581,7 +581,7 @@ namespace MSNPSharp
             {
                 if (user.Split('@')[1].ToLower(CultureInfo.InvariantCulture) == "msn.com")
                 {
-                    securService.Url = @"https://msnia.login.live.com/pp600/RST.srf";
+                    securService.Url = @"https://msnia.login.live.com/RST.srf";
                 }
             }
             else
