c621c26627e1cdb3fafc5154019a0f9e89945964
freezingsoft <freezingsoft@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-01-19 15:59:51 +0000

    Write trace output to a richtextbox
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk/MSNP-Sharp15@732 ed960e22-f52a-0410-8d30-b9dff305f3af

---
+++ Example/TraceForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MSNPSharpClient
+{
+    partial class TraceForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbTrace = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbTrace
+            // 
+            this.rtbTrace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbTrace.Location = new System.Drawing.Point(0, 0);
+            this.rtbTrace.Name = "rtbTrace";
+            this.rtbTrace.Size = new System.Drawing.Size(661, 450);
+            this.rtbTrace.TabIndex = 0;
+            this.rtbTrace.Text = "";
+            // 
+            // TraceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 450);
+            this.Controls.Add(this.rtbTrace);
+            this.Name = "TraceForm";
+            this.Text = "TraceForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbTrace;
+    }
+}
