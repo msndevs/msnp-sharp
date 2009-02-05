f7052a98d4a09db9b6d6d3a2bfe5d05c8d70e338
google@alishomepage.com <google@alishomepage.com@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-02-05 21:24:41 +0000

    Use a try ... catch instead of relying on platform-based detection
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_20_STABLE@833 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/ConnectivitySettings.cs
+++ MSNPSharp/ConnectivitySettings.cs
@@ -64,16 +64,19 @@ namespace MSNPSharp
 
         static ConnectivitySettings()
         {
-            if (null == Type.GetType("Mono.RunTime"))
+            try
             {
-                ServicePointManager.ServerCertificateValidationCallback = delegate
-                {
-                    return true;
-                };
+                ServicePointManager.ServerCertificateValidationCallback += delegate(object sender, X509Certificate certificate,
+                    X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                    {
+                        return true;
+                    };
             }
-            else
+            catch (NotImplementedException)
             {
+#pragma warning disable 0618
                 ServicePointManager.CertificatePolicy = new MSNServiceCertificatePolicy();
+#pragma warning restore 0618
             }
         }
 
