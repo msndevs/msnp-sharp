0b987fc3b1b250b224a1b4e388694bffa6980d21
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-04-03 08:11:19 +0000

    Prepare for release (2.5.5.957)
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_25_STABLE@957 ed960e22-f52a-0410-8d30-b9dff305f3af

--- Example/AssemblyInfo.cs
+++ Example/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
-[assembly: AssemblyVersionAttribute("2.5.5.939")]
+[assembly: AssemblyVersionAttribute("2.5.5.957")]
 [assembly: AssemblyTitleAttribute("MSNPSharp Example client")]
 [assembly: AssemblyDescriptionAttribute(".NET MSN Messenger library Example Client")]
 [assembly: AssemblyCopyrightAttribute("Copyright (c) 2002-2009, Bas Geertsema, Xih Solutions (http://www.xihsolutions.net), Thiago.Sayao, Pang Wu, Ethem Evlice.")]
 [assembly: AssemblyProductAttribute("MSNPSharp")]
 [assembly: AssemblyDelaySignAttribute(false)]
-[assembly: AssemblyFileVersionAttribute("2.5.5.939")]
+[assembly: AssemblyFileVersionAttribute("2.5.5.957")]
 [assembly: GuidAttribute("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC")]
