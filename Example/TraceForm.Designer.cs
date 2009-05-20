87c30ff7912270312cc439293afa2dd7f7317060
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-05-20 10:20:29 +0000

    Merge 1035
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk/MSNP-Sharp15@1036 ed960e22-f52a-0410-8d30-b9dff305f3af

--- Example/TraceForm.Designer.cs
+++ Example/TraceForm.Designer.cs
@@ -34,6 +34,9 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.tsbClear = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabelLevel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBoxLevel = new System.Windows.Forms.ToolStripComboBox();
             this.rtbTrace = new System.Windows.Forms.RichTextBox();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -56,7 +59,7 @@
             this.tsbStop.Image = ((System.Drawing.Image)(resources.GetObject("tsbStop.Image")));
             this.tsbStop.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbStop.Name = "tsbStop";
-            this.tsbStop.Size = new System.Drawing.Size(33, 22);
+            this.tsbStop.Size = new System.Drawing.Size(35, 22);
             this.tsbStop.Text = "Stop";
             this.tsbStop.ToolTipText = "Stop Tracing";
             this.tsbStop.Click += new System.EventHandler(this.tsbStop_Click);
@@ -72,7 +75,7 @@
             this.tsbClear.Image = ((System.Drawing.Image)(resources.GetObject("tsbClear.Image")));
             this.tsbClear.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbClear.Name = "tsbClear";
-            this.tsbClear.Size = new System.Drawing.Size(36, 22);
+            this.tsbClear.Size = new System.Drawing.Size(38, 22);
             this.tsbClear.Text = "Clear";
             this.tsbClear.ToolTipText = "Clear All";
             this.tsbClear.Click += new System.EventHandler(this.tsbClear_Click);
@@ -83,33 +86,61 @@
             this.tsbStart,
             this.tsbStop,
             this.toolStripSeparator1,
-            this.tsbClear});
+            this.tsbClear,
+            this.toolStripSeparator2,
+            this.toolStripLabelLevel,
+            this.toolStripComboBoxLevel});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(661, 25);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabelLevel
+            // 
+            this.toolStripLabelLevel.Name = "toolStripLabelLevel";
+            this.toolStripLabelLevel.Size = new System.Drawing.Size(40, 22);
+            this.toolStripLabelLevel.Text = "Level: ";
+            // 
+            // toolStripComboBoxLevel
+            // 
+            this.toolStripComboBoxLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.toolStripComboBoxLevel.Items.AddRange(new object[] {
+            "Verbose",
+            "Info",
+            "Warning",
+            "Error",
+            "Off"});
+            this.toolStripComboBoxLevel.Name = "toolStripComboBoxLevel";
+            this.toolStripComboBoxLevel.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBoxLevel.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBoxLevel_SelectedIndexChanged);
+            // 
             // rtbTrace
             // 
             this.rtbTrace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtbTrace.Location = new System.Drawing.Point(0, 28);
+            this.rtbTrace.Location = new System.Drawing.Point(0, 30);
             this.rtbTrace.Name = "rtbTrace";
-            this.rtbTrace.Size = new System.Drawing.Size(661, 422);
+            this.rtbTrace.Size = new System.Drawing.Size(661, 457);
             this.rtbTrace.TabIndex = 0;
             this.rtbTrace.Text = "";
             // 
             // TraceForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(661, 450);
+            this.ClientSize = new System.Drawing.Size(661, 488);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.rtbTrace);
             this.Name = "TraceForm";
             this.Text = "TraceForm";
+            this.Load += new System.EventHandler(this.TraceForm_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -125,6 +156,9 @@
         private System.Windows.Forms.ToolStripButton tsbClear;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.RichTextBox rtbTrace;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabelLevel;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxLevel;
 
 
     }
