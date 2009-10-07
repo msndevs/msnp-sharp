c463ffa05e0b98e8e35eafcee9595221e86f4b98
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-10-07 18:37:48 +0000

    Prepare for 3.0.RC
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_30_STABLE@1312 ed960e22-f52a-0410-8d30-b9dff305f3af

--- Example/DotMSNClient.Designer.cs
+++ Example/DotMSNClient.Designer.cs
@@ -691,7 +691,7 @@ namespace MSNPSharpClient
             this.Controls.Add(this.OwnerPanel);
             this.Controls.Add(this.pictureBox);
             this.Name = "ClientForm";
-            this.Text = "MSNPSharp Example Client for MSNP18 (3.0 Beta 5, r1301)";
+            this.Text = "MSNPSharp Example Client for MSNP18 (3.0 RC, r1313)";
             this.ListPanel.ResumeLayout(false);
             this.SortPanel.ResumeLayout(false);
             this.SortPanel.PerformLayout();
--- MSNPSharp/Properties/AssemblyInfo.cs
+++ MSNPSharp/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 // Major.Minor.Build.SVNRevision
-[assembly: AssemblyVersionAttribute("3.0.0.1301")]
-[assembly: AssemblyFileVersionAttribute("3.0.0.1301")]
+[assembly: AssemblyVersionAttribute("3.0.0.1313")]
+[assembly: AssemblyFileVersionAttribute("3.0.0.1313")]
