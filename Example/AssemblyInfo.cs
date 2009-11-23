b4e21f1e5897d35139adaac2e9056bf83e83815f
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2009-11-23 09:26:46 +0000

    Back to 3.0+ stable.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_30_STABLE@1435 ed960e22-f52a-0410-8d30-b9dff305f3af

--- Example/AssemblyInfo.cs
+++ Example/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 // Major.Minor.Build.SVNRevision
-[assembly: AssemblyVersionAttribute("3.0.0.1417")]
-[assembly: AssemblyFileVersionAttribute("3.0.0.1417")]
+[assembly: AssemblyVersionAttribute("3.0.2.1435")]
+[assembly: AssemblyFileVersionAttribute("3.0.2.1435")]
