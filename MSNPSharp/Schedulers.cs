02381c2276f68c25443fa1bb81c6d667b01aa5c8
freezingsoft <freezingsoft@ed960e22-f52a-0410-8d30-b9dff305f3af>
2010-02-03 07:05:24 +0000

    Improve schedulers.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk/MSNPSHARP_DEV@1488 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/Schedulers.cs
+++ MSNPSharp/Schedulers.cs
@@ -1,59 +1,171 @@
+#region Copyright (c) 2002-2010, Bas Geertsema, Xih Solutions (http://www.xihsolutions.net), Thiago.Sayao, Pang Wu, Ethem Evlice
+/*
+Copyright (c) 2002-2010, Bas Geertsema, Xih Solutions
+(http://www.xihsolutions.net), Thiago.Sayao, Pang Wu, Ethem Evlice.
+All rights reserved. http://code.google.com/p/msnp-sharp/
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the names of Bas Geertsema or Xih Solutions nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE. 
+*/
+#endregion
+
 using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Text;
+using System.Diagnostics;
 
 namespace MSNPSharp
 {
     using MSNPSharp.DataTransfer;
+    using MSNPSharp.Core;
+
+    /// <summary>
+    /// The queue object for scheduler.
+    /// </summary>
+    internal class SchedulerQueueObject
+    {
+        IMessageProcessor messageProcessor = null;
+        NetworkMessage message = null;
+        Guid messengerId = Guid.Empty;
+        DateTime createTime = DateTime.MinValue;
+
+        /// <summary>
+        /// The time this object created.
+        /// </summary>
+        public DateTime CreateTime
+        {
+            get { return createTime; }
+        }
+
+        /// <summary>
+        /// The <see cref="IMessageProcessor">MessageProcessor</see> who send the message.
+        /// </summary>
+        public IMessageProcessor MessageProcessor
+        {
+            get { return messageProcessor; }
+        }
+
+        /// <summary>
+        /// The message need to be sent.
+        /// </summary>
+        public NetworkMessage Message
+        {
+            get { return message; }
+        }
+
+        /// <summary>
+        /// The <see cref="Messenger"/> who schedule this message.
+        /// </summary>
+        public Guid MessengerId
+        {
+            get { return messengerId; }
+        }
+
+        private SchedulerQueueObject()
+        {
+        }
+
+        public SchedulerQueueObject(IMessageProcessor processor, NetworkMessage message, Guid owner)
+        {
+            messageProcessor = processor;
+            this.message = message;
+            messengerId = owner;
+            createTime = DateTime.Now;
+        }
+    }
+
+    internal interface IScheduler
+    {
+        int DelayTime
+        {
+            get;
+        }
+
+        void Enqueue(IMessageProcessor processor, NetworkMessage message, Guid ownerId);
+        Guid Register(Messenger messenger);
+        bool UnRegister(Guid id);
+    }
 
     /// <summary>
     /// Delay sending the p2p invitation messages, avoid p2p data transfer request a new conversation.
     /// </summary>
-    internal static class P2PInvitationScheduler
+    internal class Scheduler : IScheduler
     {
-        private static object syncObject = new object();
-        private const int delayTime = 5000; //In ms.
-        private static Timer timer = new Timer(OnTimerCallback, null, 0, DelayTime);
-        private static Queue<KeyValuePair<DateTime, KeyValuePair<P2PMessage, P2PMessageSession>>> messageQueue = new Queue<KeyValuePair<DateTime, KeyValuePair<P2PMessage, P2PMessageSession>>>();
+        private object syncObject = new object();
+        private int delayTime = 5000; //In ms.
+        private Thread timerThread = null;
+        private Queue<SchedulerQueueObject> messageQueue = new Queue<SchedulerQueueObject>();
+        private Dictionary<Guid, Messenger> messengerList = new Dictionary<Guid, Messenger>(0);
 
-        public static int DelayTime
+        public int DelayTime
         {
             get { return delayTime; }
-        } 
-
+        }
 
         #region Private method
 
-        private static void EnqueueMessage(P2PMessageSession p2pSession, P2PMessage message)
+        protected virtual void EnqueueMessage(IMessageProcessor processor, NetworkMessage message, Guid ownerId)
         {
-            if (p2pSession == null || message == null)
+            if (processor == null || message == null)
                 return;
 
             lock (syncObject)
             {
-
-                messageQueue.Enqueue(new KeyValuePair<DateTime, KeyValuePair<P2PMessage, P2PMessageSession>>(
-                    DateTime.Now, new KeyValuePair<P2PMessage, P2PMessageSession>(message, p2pSession)));
+                if (messengerList.ContainsKey(ownerId))
+                {
+                    messageQueue.Enqueue(new SchedulerQueueObject(processor, message, ownerId));
+                }
             }
         }
 
-        private static void OnTimerCallback(object state)
+        protected virtual void OnTimerCallback(object state)
         {
-            lock (syncObject)
+            while (true)
             {
-                if (messageQueue.Count == 0)
-                    return;
-
                 DateTime currentTime = DateTime.Now;
                 TimeSpan span = new TimeSpan(0, 0, 0, 0, DelayTime);
 
-                while (messageQueue.Count > 0 && currentTime - messageQueue.Peek().Key >= span)
+                lock (syncObject)
                 {
-                    KeyValuePair<DateTime, KeyValuePair<P2PMessage, P2PMessageSession>> item = messageQueue.Dequeue();
+                    while (messageQueue.Count > 0 && currentTime - messageQueue.Peek().CreateTime >= span)
+                    {
+                        SchedulerQueueObject item = messageQueue.Dequeue();
+                        if (messengerList.ContainsKey(item.MessengerId) && messengerList[item.MessengerId].Connected)
+                        {
+                            item.MessageProcessor.SendMessage(item.Message);
+                        }
+                    }
 
-                    item.Value.Value.SendMessage(item.Value.Key);
+
+                    if (messageQueue.Count == 0)
+                    {
+                        return;
+                    }
                 }
+
+                Thread.Sleep(span);
             }
         }
 
@@ -61,16 +173,93 @@ namespace MSNPSharp
 
         #region Public Method
 
+        public Scheduler(int delay)
+        {
+            delayTime = delay;
+            timerThread = new Thread(new ParameterizedThreadStart(OnTimerCallback));
+        }
+
         /// <summary>
         /// Add the message to sending queue.
         /// </summary>
-        /// <param name="p2pSession"></param>
+        /// <param name="processor"></param>
         /// <param name="message"></param>
-        public static void Enqueue(P2PMessageSession p2pSession, P2PMessage message)
+        /// <param name="ownerId"></param>
+        public virtual void Enqueue(IMessageProcessor processor, NetworkMessage message, Guid ownerId)
         {
-            EnqueueMessage(p2pSession, message);
+            lock (syncObject)
+            {
+                if (messengerList.ContainsKey(ownerId))
+                {
+                    if (timerThread.ThreadState == System.Threading.ThreadState.Unstarted ||
+                        timerThread.ThreadState == System.Threading.ThreadState.Stopped)
+                    {
+                        timerThread = new Thread(new ParameterizedThreadStart(OnTimerCallback));
+                        timerThread.Start(DelayTime);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            EnqueueMessage(processor, message, ownerId);
+        }
+
+        public virtual Guid Register(Messenger messenger)
+        {
+            lock (syncObject)
+            {
+                foreach (Guid guid in messengerList.Keys)
+                {
+                    if (object.ReferenceEquals(messenger, messengerList[guid]))
+                    {
+                        return guid;
+                    }
+                }
+
+                Guid newId = Guid.NewGuid();
+                while (messengerList.ContainsKey(newId))
+                {
+                    newId = Guid.NewGuid();
+                }
+
+                messengerList[newId] = messenger;
+                return newId;
+            }
+        }
+
+        public virtual bool UnRegister(Guid id)
+        {
+            lock (syncObject)
+            {
+                return messengerList.Remove(id);
+            }
         }
 
         #endregion
+    }
+
+    internal static class Schedulers
+    {
+        private static Scheduler p2pInvitationScheduler = new Scheduler(5000);
+        private static Scheduler sbRequestScheduler = new Scheduler(1000);
+
+        internal static Scheduler SwitchBoardRequestScheduler
+        {
+            get 
+            { 
+                return Schedulers.sbRequestScheduler; 
+            }
+        }
+
+        internal static Scheduler P2PInvitationScheduler
+        {
+            get 
+            { 
+                return Schedulers.p2pInvitationScheduler; 
+            }
+        }
     }
 }
