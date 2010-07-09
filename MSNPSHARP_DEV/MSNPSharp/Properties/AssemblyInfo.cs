fccb806cef77b36906368d2387f17438852c48f0
freezingsoft <freezingsoft@ed960e22-f52a-0410-8d30-b9dff305f3af>
2010-07-09 06:56:55 +0000

    Update the library version.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk@1639 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSHARP_DEV/MSNPSharp/Properties/AssemblyInfo.cs
+++ MSNPSHARP_DEV/MSNPSharp/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliantAttribute(false)]
-[assembly: AssemblyTitleAttribute("MSNPSharp")]
+[assembly: AssemblyTitleAttribute("MSNPSharp MSN Protocol Library")]
 [assembly: AssemblyDescriptionAttribute("C# implementation of the MSN protocol")]
 [assembly: AssemblyCopyrightAttribute("Copyright (c) 2002-2010, Bas Geertsema, Xih Solutions (http://www.xihsolutions.net), Thiago.Sayao, Pang Wu, Ethem Evlice.")]
 [assembly: AssemblyProductAttribute("MSNPSharp")]
@@ -20,5 +20,7 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 // Major.Minor.Build.SVNRevision
-[assembly: AssemblyVersionAttribute("3.1.2.1564")]
-[assembly: AssemblyFileVersionAttribute("3.1.2.1564")]
+[assembly: AssemblyVersionAttribute("3.1.2.1638")]
+[assembly: AssemblyFileVersionAttribute("3.1.2.1638")]
+[assembly: AssemblyCompanyAttribute("MSNPSharp")]
+[assembly: AssemblyTrademarkAttribute("MSNPSharp")]
