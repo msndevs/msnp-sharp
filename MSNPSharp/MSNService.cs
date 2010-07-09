d31db9b31b46b2e77e1d4d96a2db1f28d6c86871
freezingsoft <freezingsoft@ed960e22-f52a-0410-8d30-b9dff305f3af>
2010-07-09 15:03:21 +0000

    Fix the bug if redirection URL returns available content, the library will throw an exception.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk/MSNPSHARP_DEV@1642 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/MSNService.cs
+++ MSNPSharp/MSNService.cs
@@ -601,7 +601,21 @@ namespace MSNPSharp
                 {
                     if (originalHost != null && originalHost != String.Empty)
                     {
-                        ws.Url = originalUrl.Replace(originalHost, FetchHost(deltas.PreferredHosts[preferredHostKey]));
+                        if (deltas.PreferredHosts.ContainsKey(preferredHostKey))
+                        {
+                            ws.Url = originalUrl.Replace(originalHost, FetchHost(deltas.PreferredHosts[preferredHostKey]));
+                        }
+                        else
+                        {
+                            //This means the redirection URL returns respond content.
+                            lock (deltas.SyncObject)
+                            {
+                                deltas.PreferredHosts[preferredHostKey] = ws.Url;
+                                deltas.Save();
+                            }
+
+                            Trace.WriteLineIf(Settings.TraceSwitch.TraceInfo, "The redirect URL returns correct result, use " + ws.Url + " for " + preferredHostKey);
+                        }
                     }
 
                     // Set cache key
