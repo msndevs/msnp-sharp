8fa3675e59fef1d47a23927206555e050b28ac37
freezingsoft <freezingsoft@ed960e22-f52a-0410-8d30-b9dff305f3af>
2011-03-02 22:56:04 +0000

    Merge r2016 to 3.1 stable.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_31_STABLE@2062 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/Services/ContactService.cs
+++ MSNPSharp/Services/ContactService.cs
@@ -868,7 +868,7 @@ namespace MSNPSharp
                         {
                             if ((recursiveCall == 0 && ((MsnServiceState)e.UserState).PartnerScenario == PartnerScenario.Initial
                                 && (abHandle == null || abHandle.ABId == WebServiceConstants.MessengerIndividualAddressBookId))
-                                || (e.Error.Message.Contains("Need to do full sync")))
+                                || (e.Error.Message.Contains("Need to do full sync") || e.Error.Message.Contains("Full sync required")))
                             {
                                 Trace.WriteLineIf(Settings.TraceSwitch.TraceError,
                                     "Need to do full sync of current addressbook list, addressbook list will be request again. Method: ABFindContactsPaged");
