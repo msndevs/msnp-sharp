96832b58bb79b7e4bd16dc3ef8076bf283d3960d
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2011-03-14 20:52:12 +0000

    Move slp related methods to slp handler.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk@2131 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSHARP_DEV/MSNPSharp/P2P/SLPHandler.cs
+++ MSNPSHARP_DEV/MSNPSharp/P2P/SLPHandler.cs
@@ -39,12 +39,108 @@ namespace MSNPSharp.P2P
 {
     public class SLPHandler : IDisposable
     {
-        protected SLPHandler(int queueSize)
+        private NSMessageHandler nsMessageHandler = null;
+
+        protected internal SLPHandler(NSMessageHandler nsHandler)
         {
+            this.nsMessageHandler = nsHandler;
         }
 
         public virtual void Dispose()
         {
         }
+
+        internal P2PSession ProcessSLPMessage(P2PBridge bridge, Contact source, Guid sourceGuid, P2PMessage msg, SLPMessage slp)
+        {
+            SLPRequestMessage req = slp as SLPRequestMessage;
+
+            if (req != null && req.Method == "INVITE" &&
+                req.ContentType == "application/x-msnmsgr-sessionreqbody")
+            {
+                // Start a new session
+                return new P2PSession(req, msg, nsMessageHandler, bridge);
+            }
+
+            return null;
+        }
+
+        internal bool CheckSLPMessage(P2PBridge bridge, Contact source, Guid sourceGuid, P2PMessage msg, SLPMessage slp)
+        {
+            string src = source.Account.ToLowerInvariant();
+            string target = nsMessageHandler.Owner.Account.ToLowerInvariant();
+
+            if (msg.Version == P2PVersion.P2PV2)
+            {
+                src += ";" + sourceGuid.ToString("B").ToLowerInvariant();
+                target += ";" + NSMessageHandler.MachineGuid.ToString("B").ToLowerInvariant();
+            }
+
+            if (slp.Source.ToLowerInvariant() != src)
+            {
+                Trace.WriteLineIf(Settings.TraceSwitch.TraceWarning,
+                    String.Format("Received message from '{0}', differing from source '{1}'", slp.Source, src), GetType().Name);
+
+                return false;
+            }
+            else if (slp.Target.ToLowerInvariant() != target)
+            {
+                Trace.WriteLineIf(Settings.TraceSwitch.TraceWarning,
+                    String.Format("Received P2P message intended for '{0}', not us '{1}'", slp.Target, target), GetType().Name);
+
+                if (slp.Source == target)
+                {
+                    Trace.WriteLineIf(Settings.TraceSwitch.TraceWarning,
+                        "We received a message from ourselves?", GetType().Name);
+                }
+                else
+                {
+                    SendSLPStatus(bridge, msg, source, sourceGuid, 404, "Not Found");
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SendSLPStatus(P2PBridge bridge, P2PMessage msg, Contact dest, Guid destGuid, int code, string phrase)
+        {
+            string target = dest.Account.ToLowerInvariant();
+
+            if (msg.Version == P2PVersion.P2PV2)
+            {
+                target += ";" + destGuid.ToString("B");
+            }
+
+            SLPMessage slp = new SLPStatusMessage(target, code, phrase);
+
+            if (msg.IsSLPData)
+            {
+                SLPMessage msgSLP = msg.InnerMessage as SLPMessage;
+                slp.Branch = msgSLP.Branch;
+                slp.CallId = msgSLP.CallId;
+                slp.Source = msgSLP.Target;
+                slp.ContentType = msgSLP.ContentType;
+            }
+            else
+                slp.ContentType = "null";
+
+            P2PMessage response = new P2PMessage(msg.Version);
+            response.InnerMessage = slp;
+
+            if (msg.Version == P2PVersion.P2PV1)
+            {
+                response.V1Header.Flags = P2PFlag.MSNSLPInfo;
+            }
+            else if (msg.Version == P2PVersion.P2PV2)
+            {
+                response.V2Header.OperationCode = (byte)OperationCode.None;
+                response.V2Header.TFCombination = TFCombination.First;
+            }
+
+            bridge.Send(null, dest, destGuid, response, null);
+        }
+
+
     }
 };
