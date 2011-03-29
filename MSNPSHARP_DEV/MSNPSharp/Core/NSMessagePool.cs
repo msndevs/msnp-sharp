d4d17afed359752781c5d39903678e9c2cc8ac71
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2011-03-29 22:12:17 +0000

    This is better. Process known payload error codes as payload.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk@2278 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSHARP_DEV/MSNPSharp/Core/NSMessagePool.cs
+++ MSNPSHARP_DEV/MSNPSharp/Core/NSMessagePool.cs
@@ -191,10 +191,6 @@ namespace MSNPSharp.Core
                             (byte)bufferStream.ReadByte() 
                         });
 
-                        int errorCode3;
-                        if (int.TryParse(cmd3, out errorCode3) && errorCode3.ToString().Length == 3)
-                            cmd3 = "000";
-
                         switch (cmd3)
                         {
                             case "SDG": // SDG SendDataGram
@@ -213,7 +209,21 @@ namespace MSNPSharp.Core
                             case "IPG": // IPG pager command                            
                             case "FSL": // FSL
 
-                            case "000": // Assume all error codes payload
+                            case "201": // 201
+                            case "203": // 203
+                            case "204": // 204 Invalid contact network in ADL/RML
+                            case "205": // 205
+                            case "210": // 210
+                            case "234": // 234
+                            case "241": // 241 Invalid membership for ADL/RML
+                            case "508": // 508
+                            case "509": // 509 UpsFailure, when sending mobile message
+                            case "511": // 511
+                            case "591": // 591
+                            case "731": // 731
+                            case "801": // 801
+                            case "933": // 933
+                            case "000": // Assume all error codes payload, comes from case default...
                                 {
                                     bufferStream.Seek(-3, SeekOrigin.End);
 
@@ -245,7 +255,9 @@ namespace MSNPSharp.Core
                                     else
                                     {
                                         bufferStream.Position = 0;
-                                        goto default;
+
+                                        EnqueueCurrentBuffer();
+                                        CreateNewBuffer();
                                     }
 
                                 }
@@ -253,6 +265,10 @@ namespace MSNPSharp.Core
 
                             default:
                                 {
+                                    int errorCode3;
+                                    if (int.TryParse(cmd3, out errorCode3) && errorCode3.ToString().Length == 3)
+                                        goto case "000";
+
                                     // it was just a plain command start a new message
                                     EnqueueCurrentBuffer();
                                     CreateNewBuffer();
