45e1f54a7bf7f916d5e3c63e8e9e6f097afd2c7a
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2011-04-11 09:23:20 +0000

    Prepare for 3.1.4.2305. This is the last version of 3.X and end of support. This version fixes a bug circles can't appear in contact list and can't chat. Please upgrade your version to v4.X as soon as possible to use new features. Latest MSNP21 is the best protocol. It is pure clean and easy.
    New features of 4.x: http://code.google.com/p/msnp-sharp/wiki/MSNPSharp_v4X
    * Add support for MSNP21 http://code.google.com/p/msnp-sharp/wiki/KB_MSNP21
    * Revamped P2P framework to support P2PApplication. So, everybody can write their own P2P applications like WebCam, Games, etc.
    * Add support for Direct Connect Data Transfer for P2Pv2 clients.
    * Add support for Wink (receive only)
    * Add support for Facebook chat.
    
    
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_31_STABLE@2305 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/Properties/AssemblyInfo.cs
+++ MSNPSharp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 // Major.Minor.Build.SVNRevision
-[assembly: AssemblyVersionAttribute("3.1.4.2068")]
-[assembly: AssemblyFileVersionAttribute("3.1.4.2068")]
+[assembly: AssemblyVersionAttribute("3.1.4.2305")]
+[assembly: AssemblyFileVersionAttribute("3.1.4.2305")]
 [assembly: AssemblyCompanyAttribute("MSNPSharp")]
 [assembly: AssemblyTrademarkAttribute("MSNPSharp")]
