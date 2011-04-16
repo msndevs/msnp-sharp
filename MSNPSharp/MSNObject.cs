msndevs/msnp-sharp

commit 10e03a0bca8dcbdf30da45f9f118db58d7dbde84

Parse the Xml and replacing the space with '+' in MSNObject.
git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_40_STABLE@2324 ed960e22-f52a-0410-8d30-b9dff305f3af

MSNPSharp/MSNObject.cs
@@ -358,11 +358,6 @@ namespace MSNPSharp
         }
 
         /// <summary>
-        /// 
-        /// </summary>
-        private static Regex contextRe = new Regex("(?<Name>[^= ]+)=\"(?<Value>[^\"]+)\"");
-
-        /// <summary>
         /// Parses a context send by the remote contact and set the corresponding class variables. Context input is assumed to be not base64 encoded.
         /// </summary>
         /// <param name="context"></param>
@@ -385,14 +380,16 @@ namespace MSNPSharp
             string xmlString = context;
             if (context.IndexOf(" ") == -1)
                 xmlString = GetDecodeString(context);
-            MatchCollection matches = contextRe.Matches(xmlString);
 
-            foreach (Match match in matches)
+            XmlReader xmlReader = XmlReader.Create(new StringReader(xmlString));
+
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlNode node = xmlDocument.ReadNode(xmlReader);
+
+            foreach (XmlNode attr in node.Attributes)
             {
-                string name = match.Groups["Name"].Value.ToLower(System.Globalization.CultureInfo.InvariantCulture);
-                string val = match.Groups["Value"].Value;
-
-                switch (name)
+                string val = attr.Value;
+                switch (attr.Name.ToLowerInvariant())
                 {
                     case "creator":
                         this.creator = val;
@@ -426,7 +423,7 @@ namespace MSNPSharp
                         this.location = val;
                         break;
                     case "sha1d":
-                        this.sha = val;
+                        this.sha = val.Replace(' ', '+');
                         break;
                 }
             }

commit 0d2019c61e588ba2db742b88bb1efb786c47bcfe

Fix the XMLException issue introduced in r2324.
git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_40_STABLE@2330 ed960e22-f52a-0410-8d30-b9dff305f3af

MSNPSharp/MSNObject.cs
@@ -373,20 +373,16 @@ namespace MSNPSharp
         /// <param name="base64Encoded"></param>
         public virtual void SetContext(string context, bool base64Encoded)
         {
-
             if (base64Encoded)
                 context = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(context));
 
-            string xmlString = context;
-            if (context.IndexOf(" ") == -1)
-                xmlString = GetDecodeString(context);
+            string xmlString = GetDecodeString(context);
 
-            XmlReader xmlReader = XmlReader.Create(new StringReader(xmlString));
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlString);
+            XmlNode msnObjNode = xmlDoc.SelectSingleNode("//msnobj");
 
-            XmlDocument xmlDocument = new XmlDocument();
-            XmlNode node = xmlDocument.ReadNode(xmlReader);
-
-            foreach (XmlNode attr in node.Attributes)
+            foreach (XmlNode attr in msnObjNode.Attributes)
             {
                 string val = attr.Value;
                 switch (attr.Name.ToLowerInvariant())