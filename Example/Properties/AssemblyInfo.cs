aa051889e569239d482e8ee20c2481db1ef7637c
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2011-11-10 13:48:20 +0000

    Prepare for 4.0.4.2562
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_40_STABLE@2561 ed960e22-f52a-0410-8d30-b9dff305f3af

--- Example/Properties/AssemblyInfo.cs
+++ Example/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 // Major.Minor.Build.SVNRevision
-[assembly: AssemblyVersionAttribute("4.0.3.2539")]
-[assembly: AssemblyFileVersionAttribute("4.0.3.2539")]
+[assembly: AssemblyVersionAttribute("4.0.4.2562")]
+[assembly: AssemblyFileVersionAttribute("4.0.4.2562")]
 [assembly: AssemblyCompanyAttribute("MSNPSharp")]
 [assembly: AssemblyTrademarkAttribute("MSNPSharp")]
