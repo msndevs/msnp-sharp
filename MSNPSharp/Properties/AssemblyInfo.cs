e45b58bdaf754e1bf11e67775b3a52a8e9d008ff
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2011-11-14 07:26:11 +0000

    Prepare for 4.5.1.2582.
    git-svn-id: https://msnp-sharp.googlecode.com/svn/branches/MSNPSHARP_45_STABLE@2581 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/Properties/AssemblyInfo.cs
+++ MSNPSharp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 // Major.Minor.Build.SVNRevision
-[assembly: AssemblyVersionAttribute("4.5.0.2565")]
-[assembly: AssemblyFileVersionAttribute("4.5.0.2565")]
+[assembly: AssemblyVersionAttribute("4.5.1.2582")]
+[assembly: AssemblyFileVersionAttribute("4.5.1.2582")]
 [assembly: AssemblyCompanyAttribute("MSNPSharp")]
 [assembly: AssemblyTrademarkAttribute("MSNPSharp")]
--- MSNPSharp/Properties/Resources.Designer.cs
+++ MSNPSharp/Properties/Resources.Designer.cs
@@ -93,7 +93,7 @@ namespace MSNPSharp.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 4.5.0.2565.
+        ///   Looks up a localized string similar to 4.5.1.2582.
         /// </summary>
         internal static string DeltasListVersion {
             get {
@@ -145,7 +145,7 @@ namespace MSNPSharp.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 4.5.0.2565.
+        ///   Looks up a localized string similar to 4.5.1.2582.
         /// </summary>
         internal static string XMLContactListVersion {
             get {
