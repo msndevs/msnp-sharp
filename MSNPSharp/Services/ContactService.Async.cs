9bfc9fdcaf9c334d8fccad62e86b6d7c7f075c1d
hephappy <hephappy@ed960e22-f52a-0410-8d30-b9dff305f3af>
2011-12-05 23:31:16 +0000

    Update
    git-svn-id: https://msnp-sharp.googlecode.com/svn/trunk/MSNPSHARP_DEV@2636 ed960e22-f52a-0410-8d30-b9dff305f3af

--- MSNPSharp/Services/ContactService.Async.cs
+++ MSNPSharp/Services/ContactService.Async.cs
@@ -73,10 +73,10 @@ namespace MSNPSharp
             }
 
             FindMembershipRequestType request = new FindMembershipRequestType();
-            request.View = "Full";  // NO default!
+            request.view = "Full";  // NO default!
             request.deltasOnly = msdeltasOnly;
             request.lastChange = strLastChange;
-            request.serviceFilter = new FindMembershipRequestTypeServiceFilter();
+            request.serviceFilter = new ServiceFilter();
             request.serviceFilter.Types = new ServiceName[]
             {
                 ServiceName.Messenger,
